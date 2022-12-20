9a70a59f5282df7ee9e3b0bcbe58f75a59773d73
Simon Pineda <simonpineda0521@gmail.com>
2022-12-20 12:37:37 -0500

    the banner (with parallax effect) was finished, and the 5 layers were loaded

--- src/App.js
+++ src/App.js
@@ -1,15 +1,87 @@
+import { useParallax } from 'react-scroll-parallax';
+import { ParallaxBanner } from 'react-scroll-parallax';
+import bg from './img/background.png'
+import claud from './img/clauds.png'
+import mont1 from './img/1montain.png'
+import mont2 from './img/2montain.png'
+import tree from './img/trees.png'
 
 import './App.scss';
 
-function App() {
+const App = () => {
+  const background: BannerLayer = {
+    image: bg,
+    translateY: [20, 50],
+    opacity:[1,0.4],
+    // speed: -20 ,
+    shouldAlwaysCompleteAnimation: true,
+  };
+
+  // const montains1: BannerLayer = {
+  //   translateY: [0, 30],
+  //   scale: [1, 1.05, 'easeOutCubic'],
+  //   shouldAlwaysCompleteAnimation: true,
+  //   expanded: false,
+  //   children: (
+  //     <div className="absolute inset-0 flex items-center justify-center">
+  //       <h1 className="text-6xl md:text-8xl text-white font-thin">
+  //         Hello World!
+  //       </h1>
+  //     </div>
+  //   ),
+  // };
+
+  const clouds: BannerLayer = {
+    image: claud,
+    translateX: [-50, 90],
+    translateY: [30, 80],
+    shouldAlwaysCompleteAnimation: true,
+  };
+  const clouds2: BannerLayer = {
+    image: claud,
+    translateX: [0, 150],
+    translateY: [30, 80],
+    // translateY: [10, 20],
+    shouldAlwaysCompleteAnimation: true,
+  };
+
+  const montains1: BannerLayer = {
+    image: mont1,
+    translateY: [10, 40],
+    shouldAlwaysCompleteAnimation: true,
+  };
+
+  const montains2: BannerLayer = {
+    image:mont2,
+    // translateY: [2, 0],
+    translateY: [2, 20],
+    shouldAlwaysCompleteAnimation: true,
+  };
+
+  const trees: BannerLayer = {
+    image:tree,
+    // translateY: [0, -9],
+    speed: -2,
+    shouldAlwaysCompleteAnimation: true,
+  };
+
+  // const gradientOverlay: BannerLayer = {
+  //   opacity: [0, 0.9],
+  //   shouldAlwaysCompleteAnimation: true,
+  //   expanded: false,
+  //   children: (
+  //     <div className="absolute inset-0 bg-gradient-to-t from-gray-900 to-blue-900" />
+  //   ),
+  // };
   return (
-    <div className="App">
-      <div>
-          <h1>hola
-          </h1>
-      </div>
+    <div className='containerStart'>
+      <ParallaxBanner
+      layers={[background, clouds, clouds2, montains1, montains2, trees]}
+      className="banner"
+    />
     </div>
   );
-}
+};
 
 export default App;
+
--- src/index.js
+++ src/index.js
@@ -1,20 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.scss';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { ParallaxProvider } from 'react-scroll-parallax';
 
 import {RouterProvider} from "react-router-dom"
 import { router } from './router';
-
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyC6AtVb7hPaEwpTfs0Grf65Ef4Tg2w4ELc",
   authDomain: "quizy-b1cbf.firebaseapp.com",
@@ -24,19 +17,15 @@ const firebaseConfig = {
   appId: "1:860334105935:web:8cb5ba8c38f1d536e7513a",
   measurementId: "G-ZC12RYP1FW"
 };
-
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <RouterProvider router={router}/>
+    <ParallaxProvider>
+      <RouterProvider router={router}/>
+    </ParallaxProvider>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
