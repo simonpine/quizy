0cb561dcb7851568353aad3cf4c9b8debe1410e5
Simon Pineda <simonpineda0521@gmail.com>
2022-12-23 21:55:08 -0500

    The footer component in finished, and is responsive

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { useParallax } from 'react-scroll-parallax';
 import { MontainParallaxBanner } from './components/montainParallaxBanner';
 import { Navbar } from './components/navBar';
-
+import { Footer } from './components/footer';
 import './App.scss';
 
 const App = () => {
@@ -10,8 +10,8 @@ const App = () => {
       <MontainParallaxBanner />
       <div className='contentHome'>
         <Navbar selected={1} />
-        
       </div>
+      <Footer />
     </div>
   );
 };
