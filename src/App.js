2c4509b6a73d0ad2b6fb9f1f6b4d68499e478fa2
Simon Pineda <simonpineda0521@gmail.com>
2022-12-26 23:38:22 -0500

    The create image is done in animateScrollIcon, and the text is resonsive in phones

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import { useParallax } from 'react-scroll-parallax';
+
 import { MontainParallaxBanner } from './components/montainParallaxBanner';
 import { Layout } from './components/layout';
 import './App.scss';
-
+import { Link } from 'react-router-dom';
+import { AnimateScrollIcons } from './components/animateScrollIcons';
 const App = () => {
   return (
+
     <div className='containerStart'>
       <MontainParallaxBanner />
       <Layout selceted={1}>
-      <div className='contentHome'>
-        <div className='sectionHome create'>
-          <div className=''>
-            <h2 className='subtitleHome'>Create new quizzes</h2>
-            <p className='pHome'>
-              With quizy you can create quizzes that others can solve with a code, the quizzes can have many questions as you want, also it don't have to be of an specific subject. The quizzes you create cannot be modified, and will be public in the "test" section.
-            </p>
+        <div className='contentHome'>
+          <div className='sectionHome create'>
+            <div className='textContentHome'>
+              <h2 className='subtitleHome'>Create new quizzes</h2>
+              <p className='pHome'>
+                With quizy you can create quizzes that others can solve with a code, the quizzes can have many questions as you want, also it don't have to be of an specific subject. The quizzes you create cannot be modified, and will be public in the "test" section.
+              </p>
+              <Link to={{ pathname: "/create" }} className="link">New quizz</Link>
+            </div>
+            <div className='spinnerContainer'>
+              <AnimateScrollIcons />
+            </div>
+          </div>
+          <div className='sectionHome test'>
+            <div className='textContentHome2'>
+              <h2 className='subtitleHome2'>Test yourself</h2>
+              <p className='pHome'>
+                With quizy you will be able to resolve your quizzes and those of others, the grade will not be public, therefore no one will can access to it. To take a test you need to have a quizz code, or select one of the public quizzes.
+              </p>
+              <Link to={{ pathname: "/tests" }} className="link">Resolve</Link>
+            </div>
+            <div>
+              {/* <div ref={parallax.ref} className="spinner">
+                <div className="diamond">💎💎💎💎</div>
+                <div className="clown">🤡💎💎💎</div>
+                <div className="money">💰💎💎💎</div>
+                <div className="hand">👌🏻💎💎💎</div>
+              </div> */}
+            </div>
           </div>
         </div>
-      </div>
       </Layout>
     </div>
   );
---
+++ src/components/animateScrollIcons.js
@@ -0,0 +1,39 @@
+import man from '../img/man.png'
+import one from '../img/1.png'
+import two from '../img/2.png'
+import tree from '../img/3.png'
+import four from '../img/4.png'
+import { useParallax } from 'react-scroll-parallax';
+export function AnimateScrollIcons(){
+    const general = useParallax({
+        rotate: [360, 0],
+      });
+      const specific = useParallax({
+        rotate: [0, 360],
+      });
+      const specific2 = useParallax({
+        rotate: [0, 400],
+      });
+      const specific3 = useParallax({
+        rotate: [0, 400],
+      });
+      const specific4 = useParallax({
+        rotate: [0, 400],
+      });
+      const specific5 = useParallax({
+        rotate: [0, 400],
+      });
+      return(
+        <div ref={general.ref} className="spinner">
+        <img ref={specific5.ref} id='one' className='secundaryMiniIcon' src={one} />
+        <div></div>
+        <img ref={specific4.ref} id='two' className='secundaryMiniIcon' src={two} />
+        <div></div>
+        <img ref={specific.ref} className='person' src={man} />
+        <div></div>
+        <img ref={specific2.ref} id='tree' className='secundaryMiniIcon' src={tree} />
+        <div></div>
+        <img ref={specific3.ref} id='four' className='secundaryMiniIcon' src={four} />
+      </div>
+      )
+}
