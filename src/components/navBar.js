8836e498ea3ffcb9f4884d4c2c3f556338ce96ca
Simon Pineda <simonpine@Simons-MacBook-Pro.local>
2024-07-29 22:41:56 -0500

    The login method was added

--- src/components/navBar.js
+++ src/components/navBar.js
@@ -3,24 +3,35 @@ import { useLocation } from 'react-router-dom';
 import logo from '../img/logo1.svg'
 import { useState, useEffect } from "react"
 import { useKey } from "react-use";
-function Navbar({setMenu, menu}) {
+import { auth } from '../firebasecom'
+import { useAuthState } from 'react-firebase-hooks/auth'
+import { GoogleAuthProvider } from "firebase/auth";
+function Navbar({ setMenu, menu }) {
     return (
-    <nav>
-        <NavLink className='navlinkGeneral' to='/'>
-            <img className="logoNav" src={logo} alt="SimonPine brand logo" />
-        </NavLink>
-        <button className="buttonMain" onClick={() => setMenu(!menu)}>
-            {!menu ? 'Menu' : 'Close'}
-        </button>
-    </nav>
+        <nav>
+            <NavLink className='navlinkGeneral' to='/'>
+                <img className="logoNav" src={logo} alt="SimonPine brand logo" />
+            </NavLink>
+            <button className="buttonMain" onClick={() => setMenu(!menu)}>
+                {!menu ? 'Menu' : 'Close'}
+            </button>
+        </nav>
     )
 }
 
 export function Nav() {
+    const [user] = useAuthState(auth)
     const [menu, setMenu] = useState(false)
     const location = useLocation();
-    useKey('Escape', ()=>{setMenu(false)});    
-
+    useKey('Escape', () => { setMenu(false) });
+    function signInWithGoogle (){
+        auth.signOut()
+        const provider = new GoogleAuthProvider()
+        auth.signInWithPopup(provider)
+    }
+    useEffect(()=>{
+        console.log(user)
+    }, [user])
     useEffect(() => {
         setMenu(false)
     }, [location]);
@@ -113,6 +124,71 @@ export function Nav() {
                                 }}
                             </NavLink>
                         </li>
+                        {!user ?
+                            <li className="nav-li lastDelay">
+
+                                    <button onClick={signInWithGoogle} className="nav-a">
+                                        <span className="nav-a-letters">
+                                            <span className={`nav-a-letter`}> S</span>
+                                            <span className={`nav-a-letter`}> i</span>
+                                            <span className={`nav-a-letter`}> g</span>
+                                            <span className={`nav-a-letter`}> n</span>
+                                            <span className={`nav-a-letter`}> -</span>
+                                            <span className={`nav-a-letter`}> i</span>
+                                            <span className={`nav-a-letter`}> n</span>
+
+                                        </span>
+                                        <span className="nav-a-stripe nav-a-stripe--yellow"> </span>
+                                        <span className="nav-a-stripe nav-a-stripe--turquoise"> </span>
+                                        <span className="nav-a-stripe nav-a-stripe--purple"> </span>
+                                        <span className="nav-a-letters-top">
+                                            <span className={`nav-a-letter`}> S</span>
+                                            <span className={`nav-a-letter`}> i</span>
+                                            <span className={`nav-a-letter`}> g</span>
+                                            <span className={`nav-a-letter`}> n</span>
+                                            <span className={`nav-a-letter`}> -</span>
+                                            <span className={`nav-a-letter`}> i</span>
+                                            <span className={`nav-a-letter`}> n</span>
+                                        </span>
+                                    </button>
+
+                                {/* </button> */}
+                            </li>
+                            :
+                            <li className="nav-li lastDelay">
+                                <NavLink to='/tests'>
+                                    {({ isActive }) => {
+                                        return (
+                                            <div className="nav-a">
+                                                <span className="nav-a-letters">
+                                                    <span className={`nav-a-letter ${isActive && 'activelink'}`}> S</span>
+                                                    <span className={`nav-a-letter ${isActive && 'activelink'}`}> e</span>
+                                                    <span className={`nav-a-letter ${isActive && 'activelink'}`}> t</span>
+                                                    <span className={`nav-a-letter ${isActive && 'activelink'}`}> t</span>
+                                                    <span className={`nav-a-letter ${isActive && 'activelink'}`}> i</span>
+                                                    <span className={`nav-a-letter ${isActive && 'activelink'}`}> n</span>
+                                                    <span className={`nav-a-letter ${isActive && 'activelink'}`}> g</span>
+                                                    <span className={`nav-a-letter ${isActive && 'activelink'}`}> s</span>
+                                                </span>
+                                                <span className="nav-a-stripe nav-a-stripe--yellow"> </span>
+                                                <span className="nav-a-stripe nav-a-stripe--turquoise"> </span>
+                                                <span className="nav-a-stripe nav-a-stripe--purple"> </span>
+                                                <span className="nav-a-letters-top">
+                                                <span className={`nav-a-letter ${isActive && 'activelink'}`}> S</span>
+                                                    <span className={`nav-a-letter ${isActive && 'activelink'}`}> e</span>
+                                                    <span className={`nav-a-letter ${isActive && 'activelink'}`}> t</span>
+                                                    <span className={`nav-a-letter ${isActive && 'activelink'}`}> t</span>
+                                                    <span className={`nav-a-letter ${isActive && 'activelink'}`}> i</span>
+                                                    <span className={`nav-a-letter ${isActive && 'activelink'}`}> n</span>
+                                                    <span className={`nav-a-letter ${isActive && 'activelink'}`}> g</span>
+                                                    <span className={`nav-a-letter ${isActive && 'activelink'}`}> s</span>
+                                                </span>
+                                            </div>
+                                        )
+                                    }}
+                                </NavLink>
+                            </li>
+                        }
                     </ul>
                 </aside>
             }
--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import reportWebVitals from './reportWebVitals';
 
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+// import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
 import './index.scss';
 
 import { ParallaxProvider } from 'react-scroll-parallax';
